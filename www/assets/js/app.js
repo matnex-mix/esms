0aeb32eb219bb53a6cc5906c7303a730c70a022a
matnex-mix <jolaoshobatmat@gmail.com>
2020-04-08 14:53:55 +0100

    U

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -152,3 +152,23 @@ function select( el ){
 	e.classList.add( 'd-none' );
 
 }
+
+function sendMessage(){
+
+	to = $('#to').val();
+	msg = $('#message').val();
+
+	console.log( msg );
+	var options = {
+        replaceLineBreaks: false, // true to replace \n by a new line, false by default
+        android: {
+            intent: 'INTENT'  // send SMS with the native android SMS messaging
+            //intent: '' // send SMS without opening any other app
+        }
+    };
+
+    var success = function () { alert('Message sent successfully'); };
+    var error = function (e) { alert('Message Failed:' + e); };
+    if(SMS) SMS.sendSMS( to, msg, success, error );
+
+}
